5f4ae698b3da93c03a9081ec46d35be9775fff24
AGIvanov <lnh19@yandex.ru>
2023-02-20 19:15:45 +0300

    Ready to start Chapter 10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,18 +13,31 @@ class UsersController < ApplicationController
     if @user.save
       reset_session
       log_in @user
-      flash[:success] = "Welcome to Sample App"
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
   end
 
-  private
+  def edit
+    @user = User.find(params[:id])
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        render 'edit', status: :unprocessable_entity
+      end
     end
+
+    private
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
 
 end
